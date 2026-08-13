70dc004e81503b25e2f103e45df4276d070b32bb
meteo-lestatins <lestatins@free.fr>
2026-08-13 17:13:27 +0200

    Déplacer la distance des cellules sandbox

--- sandbox/service-worker.js
+++ sandbox/service-worker.js
@@ -1,5 +1,5 @@
-const cacheName = "meteo-sandbox-v7";
-const shell = ["/sandbox/runtime-config.js?v=2.061", "/sandbox/changelog.html", "/sandbox/", "/sandbox/index.html", "/sandbox/style.css?v=96", "/sandbox/app.js?v=126", "/sandbox/manifest.webmanifest", "/sandbox/vendor/leaflet/leaflet.css", "/sandbox/vendor/leaflet/leaflet.js"];
+const cacheName = "meteo-sandbox-v8";
+const shell = ["/sandbox/runtime-config.js?v=2.061", "/sandbox/changelog.html", "/sandbox/", "/sandbox/index.html", "/sandbox/style.css?v=97", "/sandbox/app.js?v=127", "/sandbox/manifest.webmanifest", "/sandbox/vendor/leaflet/leaflet.css", "/sandbox/vendor/leaflet/leaflet.js"];
 self.addEventListener("install", event => event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(shell)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
