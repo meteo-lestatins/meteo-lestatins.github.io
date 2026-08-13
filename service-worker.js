47b6777324694fef8a1dce2dc93d240fabe56aeb
meteo-lestatins <lestatins@free.fr>
2026-08-13 13:33:04 +0200

    Corriger les pastilles d'évolution

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const cacheName = "meteo-france-v64";
-const shell = ["/runtime-config.js?v=2.052", "/changelog.html", "/", "/index.html", "/style.css?v=92", "/app.js?v=117", "/manifest.webmanifest"];
+const cacheName = "meteo-france-v65";
+const shell = ["/runtime-config.js?v=2.053", "/changelog.html", "/", "/index.html", "/style.css?v=92", "/app.js?v=118", "/manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(shell)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => { if (event.request.method === "GET" && new URL(event.request.url).origin === location.origin && !new URL(event.request.url).pathname.startsWith("/api/")) event.respondWith(fetch(event.request).catch(() => caches.match(event.request))); });
