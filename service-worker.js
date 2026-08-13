3a05c659e19bec8ee2639f7924ae88c99b09ac6a
meteo-lestatins <lestatins@free.fr>
2026-08-13 15:15:59 +0200

    Revert "Réorganiser le nowcasting en deux colonnes"
    This reverts commit 16a5f21e47a1ef54bdcc57d502cf733269fec12a.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const cacheName = "meteo-france-v68";
-const shell = ["/runtime-config.js?v=2.057", "/changelog.html", "/", "/index.html", "/style.css?v=94", "/app.js?v=121", "/manifest.webmanifest", "/vendor/leaflet/leaflet.css", "/vendor/leaflet/leaflet.js"];
+const cacheName = "meteo-france-v67";
+const shell = ["/runtime-config.js?v=2.056", "/changelog.html", "/", "/index.html", "/style.css?v=93", "/app.js?v=120", "/manifest.webmanifest", "/vendor/leaflet/leaflet.css", "/vendor/leaflet/leaflet.js"];
 self.addEventListener("install", event => event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(shell)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
