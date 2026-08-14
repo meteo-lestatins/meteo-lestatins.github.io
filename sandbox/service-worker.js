50fa882f825ef372adbf847982eac8be6859d6b1
meteo-lestatins <lestatins@free.fr>
2026-08-14 13:45:23 +0200

    Réorganiser About et le numéro de version

--- sandbox/service-worker.js
+++ sandbox/service-worker.js
@@ -1,5 +1,5 @@
-const cacheName = "meteo-sandbox-v58";
-const shell = ["/sandbox/about.html", "/sandbox/architecture.svg", "/sandbox/echelle-kilometrique.css?v=7", "/sandbox/echelle-kilometrique.js?v=10", "/sandbox/runtime-config.js?v=2.094", "/sandbox/changelog.html", "/sandbox/", "/sandbox/index.html", "/sandbox/style.css?v=122", "/sandbox/app.js?v=155", "/sandbox/manifest.webmanifest", "/sandbox/vendor/leaflet/leaflet.css", "/sandbox/vendor/leaflet/leaflet.js"];
+const cacheName = "meteo-sandbox-v59";
+const shell = ["/sandbox/about.html", "/sandbox/architecture.svg", "/sandbox/echelle-kilometrique.css?v=7", "/sandbox/echelle-kilometrique.js?v=10", "/sandbox/runtime-config.js?v=2.094", "/sandbox/changelog.html", "/sandbox/", "/sandbox/index.html", "/sandbox/style.css?v=123", "/sandbox/app.js?v=155", "/sandbox/manifest.webmanifest", "/sandbox/vendor/leaflet/leaflet.css", "/sandbox/vendor/leaflet/leaflet.js"];
 self.addEventListener("install", event => event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(shell)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
