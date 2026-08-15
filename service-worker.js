ca29496b5ad2b0485442517ddf9874dd3e5b9f38
meteo-lestatins <lestatins@free.fr>
2026-08-15 09:00:40 +0200

    Republication Main v2.105

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "meteo-main-v2-105";
+const cacheName = "meteo-main-v2-105-r1";
 const shell = ["/about.html", "/architecture.svg", "/echelle-kilometrique.css?v=8", "/echelle-kilometrique.js?v=11", "/runtime-config.js?v=2.105", "/changelog.html", "/", "/index.html", "/style.css?v=124", "/app.js?v=160", "/manifest.webmanifest", "/vendor/leaflet/leaflet.css", "/vendor/leaflet/leaflet.js"];
 self.addEventListener("install", event => event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(shell)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))).then(() => self.clients.claim())));
